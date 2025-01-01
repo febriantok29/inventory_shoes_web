ba197f8ea95d57451c10f563ce70c09beac4b9cf
bimbim <febriantok29@gmail.com>
2025-01-01 17:13:41 +0700

    Fix naming label on index product UI

--- resources/views/master/products/index.blade.php
+++ resources/views/master/products/index.blade.php
@@ -1,25 +1,24 @@
 @extends('layouts.app')
 
-@section('title', 'Daftar Produk')
-
-@section('page_title', 'Daftar Produk')
+@section('title', 'Daftar Sepatu')
 
 @section('content')
     <div class="row">
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Daftar Produk</h3>
+                    <h3 class="card-title">Daftar Sepatu</h3>
                     <div class="card-tools">
-                        <a href="{{ route('products.create') }}" class="btn btn-primary">Tambah Produk</a>
+                        <a href="{{ route('products.create') }}" class="btn btn-primary"><i class="fas fa-plus"></i> Tambah
+                            Sepatu Baru</a>
                     </div>
                 </div>
                 <div class="card-body">
                     <table class="table table-bordered">
                         <thead>
                             <tr>
-                                <th>Kode</th>
-                                <th>Nama</th>
+                                <th>SKU</th>
+                                <th>Sepatu</th>
                                 <th>Kategori</th>
                                 <th>Total Stok</th>
                                 <th>Aksi</th>
@@ -39,7 +38,9 @@
                                         <a href="{{ route('products.edit', $product->id) }}" class="btn btn-warning btn-sm">
                                             <i class="fas fa-edit"></i> Edit
                                         </a>
-                                        <form action="{{ route('products.destroy', $product->id) }}" method="POST" class="d-inline" onsubmit="return confirm('Apakah Anda yakin ingin menghapus produk ini?');">
+                                        <form action="{{ route('products.destroy', $product->id) }}" method="POST"
+                                            class="d-inline"
+                                            onsubmit="return confirm('Apakah Anda yakin ingin menghapus produk ini?');">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-danger btn-sm">
